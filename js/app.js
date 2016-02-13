4276f1373da62d10e5b813fab8f29b0fecd5b99f
Brian Kemper <bkemper@gmail.com>
2016-02-12 21:48:47 -0700

    fixed many bugs, got localStorage working much better

--- js/app.js
+++ js/app.js
@@ -1,9 +1,32 @@
 $(document).ready(function() {
 
-//pulls tweet text and time fron localstorage when you refresh
-  for (var i = 1; i < localStorage.length + 1; i += 2) {
-    $('#stream').prepend('<div class="tweet"><div class="content"><img class="avatar" src="img/alagoon.jpg" /><strong class="fullname">BKing</strong><span class="username">@tasty</span><p class="tweet-text">' + localStorage.getItem(i) + '</p><div class="tweet-actions"><ul><li><span class="icon action-reply"></span> Reply</li><li><span class="icon action-retweet"></span> Retweet</li><li><span class="icon action-favorite"></span> Favorite</li><li><span class="icon action-more"></span> More</li></ul></div><div class="time">' + '<p><span data-livestamp="' + localStorage.getItem(i + 1) + '"></span></p>' + '</div></div>');
+  var date = new Date();
+
+  if (localStorage.getItem('tweetList')) {
+    var serverList = localStorage.getItem('tweetList').split(',');
+    idList = serverList;} else {idList = [];}
+
+if (idList.length) {
+    for (var i=0; i<idList.length; i++) {
+      var currentId = idList[i];
+      var currentItem = JSON.parse(localStorage.getItem(currentId));
+  $('#stream').prepend('<div class="tweet" id="' + currentItem.tweetId + '"><div class="content"><img class="avatar" src="img/alagoon.jpg" /><strong class="fullname">BKing</strong><span class="username">@tasty</span><p class="tweet-text">' + currentItem.tweetText + '</p><div class="tweet-actions"><ul><li><span class="icon action-reply"></span> Reply</li><li><span class="icon action-retweet"></span> Retweet</li><li><span class="icon action-favorite"></span> Favorite</li><li><span class="icon action-more"></span> More</li></ul></div><div class="time">' + '<p><span data-livestamp="' + currentItem.tweetTime + '"></span></p>' + '</div></div>');
+    }
   }
+
+  // $('#testEdit').on('click', function() {
+    // var currentId = IDNUMBER;
+    // var currentItem = JSON.parse(localStorage.getItem(currentId));
+    // localStorage.removeItem(IDNUMBER);
+    // var currentList = JSON.parse(localStorage.getItem(currentId));
+    // localStorage.removeItem('tweetList');
+    // idList.splice(idList.indexOf(448828),1);
+    // localStorage.setItem('tweetList', idList);
+    //allows for tweets to be edited by removing, adding properties, then adding back with same id number, will not update text without refresh, but useful for adding/toggling props
+    // delete currentItem.moreass;
+    // currentItem.TESTER = 'ideazz';
+    // localStorage.setItem(currentId, JSON.stringify(currentItem));
+  // });
 
   //initially hide all tweet actions and controls
   $('.tweet-actions').hide();
@@ -17,28 +40,26 @@ $(document).ready(function() {
     $(this).siblings('#tweet-controls').fadeIn();
   });
 
-  //count characters, disable button when too many characters are in, change to red when <10 remaining
+  //count characters, disable button when too many characters are in, disable button when no characters are typed in, change to red when <10 remaining
   var charCount = 1;
+  $('#tweet-submit').attr('disabled', 'disabled');
   $('textarea').on('keyup', function() {
     charCount = 140 - $('textarea.tweet-compose').val().length;
     $('#char-count').text(charCount);
-    if (charCount < 0) {
-      $('#char-count').css({
-        color: 'red'
-      });
-      $('#tweet-submit').attr('disabled', 'disabled');
-    } else if (charCount >= 0 && charCount <= 10) {
-      $('#char-count').css({
-        color: 'red'
-      });
-      $('#tweet-submit').removeAttr('disabled');
-    } else if (charCount > 10) {
-      $('#char-count').css({
-        color: '#999'
-      });
-    }
-  });
 
+    if (charCount === 140) {
+      $('#tweet-submit').attr('disabled', 'disabled');
+    } else if (charCount > 10 && charCount < 140) {
+      $('#char-count').css({color: '#999'});
+      $('#tweet-submit').removeAttr('disabled');
+    } else if (charCount >= 0 && charCount <= 10) {
+      $('#char-count').css({color: 'red'});
+      $('#tweet-submit').removeAttr('disabled');
+    } else  if (charCount < 0) {
+      $('#char-count').css({color: 'red'});
+      $('#tweet-submit').attr('disabled', 'disabled');
+      }
+  });
 //grab fullname, username, and tweet text when the reply button is clicked, could be used to set up a reply thing
   $('.action-retweet').parent().on('click', function() {
     var fullName = $($(this).parents()[2]).find('.fullname').text();
@@ -46,7 +67,7 @@ $(document).ready(function() {
     var tweetText = $($(this).parents()[2]).find('.tweet-text').text();
   });
 
-  //shrink textarea and fade out tweet controls on blur
+//shrink textarea and fade out tweet controls on blur
   $('textarea').blur(function() {
     $(this).animate({
       height: '2.5em'
@@ -54,27 +75,25 @@ $(document).ready(function() {
     $('#tweet-controls').fadeOut();
   });
 
-  //enclosure to up key count for localstorage, pushes tweet and time into two different items
-  var addTweetClosure = function(task) {
-    var count = localStorage.length + 1;
-    return function(task, time) {
-      localStorage.setItem(count, task);
-      localStorage.setItem(count + 1, time);
-      count += 2;
-    };
-  };
+var tweetIdMaker = function() {
+  return Math.floor(Math.random()*1000000);
+};
 
-  //addtweet enclosure function
-  var addTweet = addTweetClosure();
+var newTweet = function(tweetText, tweetTime, tweetId) {
+  var tweetIdString = tweetId.toString();
+  idList.push(tweetIdString);
+  localStorage.setItem('tweetList', idList);
+  localStorage.setItem(tweetId, JSON.stringify({tweetText: tweetText, tweetTime: tweetTime, tweetId: tweetId}));
+  $('#char-count').text('140');
+  return ('<div class="tweet" id="' + tweetId + '"><div class="content"><img class="avatar" src="img/alagoon.jpg" /><strong class="fullname">BKing</strong><span class="username">@tasty</span><p class="tweet-text">' + tweetText + '</p><div class="tweet-actions"><ul><li><span class="icon action-reply"></span> Reply</li><li><span class="icon action-retweet"></span> Retweet</li><li><span class="icon action-favorite"></span> Favorite</li><li><span class="icon action-more"></span> More</li></ul></div><div class="time">' + '<p><span data-livestamp="' + tweetTime + '"></span></p>' + '</div></div>');
+  };
 
   //prepend tweet, empty tweet box if successful, hide tweet actions on new tweet, and also the actions to show on hover over
   $('.button').on('click', function() {
-    //push tweet to local storage
-    addTweet($('textarea.tweet-compose').val(), Math.round(Date.now() / 1000));
     //if textarea is not empty
     if ($('textarea.tweet-compose').val()) {
       //prepend new tweet
-      $('#stream').prepend('<div class="tweet"><div class="content"><img class="avatar" src="img/alagoon.jpg" /><strong class="fullname">BKing</strong><span class="username">@tasty</span><p class="tweet-text">' + $('textarea.tweet-compose').val() + '</p><div class="tweet-actions"><ul><li><span class="icon action-reply"></span> Reply</li><li><span class="icon action-retweet"></span> Retweet</li><li><span class="icon action-favorite"></span> Favorite</li><li><span class="icon action-more"></span> More</li></ul></div><div class="time">' + '<p><span data-livestamp="' + Math.round(Date.now() / 1000) + '"></span></p>' + '</div></div>');
+      $('#stream').prepend(newTweet($('textarea.tweet-compose').val(), date.toISOString(), tweetIdMaker()));
     }
     //clear tweet area if tweet is sucessful
     if ($('textarea.tweet-compose').val().length <= 140) {
@@ -99,7 +118,5 @@ $(document).ready(function() {
   $('.tweet').mouseleave(function() {
     $(this).find('.tweet-actions').slideUp();
   });
-
-
 
 });
